f40ffe30e51e2ff5f545856613abbf84246f4e6e
tristanlatr <tris.la.tr@gmail.com>
2020-04-08 22:17:56 -0400

    removed random wp sites from tests

--- test.py
+++ test.py
@@ -20,18 +20,13 @@ if __name__ == '__main__':
     ( messages, warnings, alerts ) = ([],[],[])
     args=parse_args()
 
-SITES="""epoxiemktg.com
-driveinteractivepdx.com
-protodave.com
-affiliatesbootcamp.com
-serviceproeastidaho.com
-"""
+SITES=[{'url':''}]
 
 class WPWatcherTests(unittest.TestCase):
 
     def test_simple(self):
         w=WPWatcher({
-            'wp_sites' :[{'url':v} for v in SITES.splitlines()],
+            'wp_sites' :SITES,
             'false_positive_strings' : ['No WPVulnDB API Token given, as a result vulnerability data has not been output'],                        
             'wpscan_path':'wpscan',
             'log_file':"",
@@ -57,7 +52,8 @@ class WPWatcherTests(unittest.TestCase):
             'daemon_loop_sleep':timedelta(seconds=0),
             'resend_emails_after':timedelta(seconds=0),
             'wp_reports':'./test.json',
-            'asynch_workers':3
+            'asynch_workers':3,
+            'follow_redirect':False
         })
         exit_code, results=w.run_scans_and_notify()
         self.assertEqual(0, exit_code)
@@ -90,14 +86,15 @@ class WPWatcherTests(unittest.TestCase):
             'daemon_loop_sleep':timedelta(seconds=0),
             'resend_emails_after':timedelta(seconds=0),
             'wp_reports':'',
-            'asynch_workers':3
+            'asynch_workers':3,
+            'follow_redirect':False
         })
         exit_code, results=w.run_scans_and_notify()
         self.assertEqual(-1, exit_code)
 
     def test_json(self):
         w=WPWatcher({
-            'wp_sites' :[{"url":"affiliatesbootcamp.com"}, {"url":"driveinteractivepdx.com"}],
+            'wp_sites' :SITES,
             'false_positive_strings' : ['No WPVulnDB API Token given, as a result vulnerability data has not been output'],                        
             'wpscan_path':'wpscan',
             'log_file':"",
@@ -123,7 +120,8 @@ class WPWatcherTests(unittest.TestCase):
             'daemon_loop_sleep':timedelta(seconds=0),
             'resend_emails_after':timedelta(seconds=0),
             'wp_reports':'./test-parse-json.json',
-            'asynch_workers':3
+            'asynch_workers':3,
+            'follow_redirect':False
         })
         exit_code, results=w.run_scans_and_notify()
         self.assertEqual(0, exit_code)
@@ -140,7 +138,7 @@ wpscan_args=[   "--format", "cli",
 # false_positive_strings=["You can get a free API token with 50 daily requests by registering at https://wpvulndb.com/users/sign_up"]
 
 # Sites (--url or --urls)
-wp_sites=   [ {"url":"affiliatesbootcamp.com"}, {"url":"driveinteractivepdx.com"} ]
+wp_sites=%s
 
 # Notifications (--send , --em , --infos , --errors , --attach , --resend)
 send_email_report=Yes
@@ -169,7 +167,8 @@ wp_reports=./test.json
 
 # Exit if any errors (--ff)
 # fail_fast=Yes 
-"""
+
+"""%(json.dumps(SITES))
         with open('./wpwatcher.conf', 'w') as configfile:
             configfile.write(config)
         w=WPWatcher(build_config({ 'quiet':True }))
