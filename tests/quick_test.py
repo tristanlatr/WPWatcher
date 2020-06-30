87988d95ec42e72bbab6daa972c6da4496c2d3f6
tristanlatr <tris.la.tr@gmail.com>
2020-06-30 11:57:05 -0400

    Fixed parser test

--- tests/quick_test.py
+++ tests/quick_test.py
@@ -367,22 +367,27 @@ class WPWatcherTests(unittest.TestCase):
         out = open("tests/static/wordpress_no_vuln.json").read()
         messages, warnings, alerts=parse_results(out)
         self.assertEqual(0, len(alerts))
+
         out = open("tests/static/wordpress_one_vuln.json").read()
         messages, warnings, alerts=parse_results(out)
-        self.assertEqual(1, len(alerts))
+        self.assertEqual(3, len(warnings))
+
         out = open("tests/static/wordpress_many_vuln.json").read()
         messages, warnings, alerts=parse_results(out)
-        self.assertEqual(3, len(alerts))
+        self.assertEqual(1, len(alerts))
 
         out = open("tests/static/wordpress_no_vuln.txt").read()
         messages, warnings, alerts=parse_results(out)
         self.assertEqual(0, len(alerts))
+
         out = open("tests/static/wordpress_one_warning.txt").read()
         messages, warnings, alerts=parse_results(out)
         self.assertEqual(2, len(warnings))
+
         out = open("tests/static/wordpress_many_vuln.txt").read()
         messages, warnings, alerts=parse_results(out)
         self.assertEqual(8, len(alerts))
+        
         out = open("tests/static/wordpress_one_vuln.txt").read()
         messages, warnings, alerts=parse_results(out)
         self.assertEqual(1, len(alerts))
