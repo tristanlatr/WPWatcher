d9664eec5a58d3ffe809b9eba4eaa7716cd1721d
tristanlatr <tris.la.tr@gmail.com>
2020-06-30 12:02:43 -0400

    Bump to version 2.2.0

--- wpwatcher/__init__.py
+++ wpwatcher/__init__.py
@@ -11,7 +11,7 @@ import os
 # Setup configuration: will be parsed by setup.py -------------------
 # Values must be in one line
 # Project version.
-VERSION='3.dev'
+VERSION='2.2.0'
 # URL that will be displayed in help and other places
 GIT_URL="https://github.com/tristanlatr/WPWatcher"
 # Authors
