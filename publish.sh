57358297a0b27d5ccda32d28d09911020dea6f5a
tristanlatr <tris.la.tr@gmail.com>
2020-07-23 21:37:29 -0400

    Use python3 -m twine in publish script

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ rm -rf ./dist
 rm -fr ./build
 rm -fr ./wpwatcher.egg-info
 python3 setup.py build check sdist bdist_wheel
-twine upload --verbose dist/*
+python3 -m twine upload --verbose dist/*
 python3 setup.py clean
 rm -rf ./dist
 rm -fr ./build
