a16913efe0d84ede659063e05e431dbbf4ee2a24
tristanlatr <tris.la.tr@gmail.com>
2020-10-05 07:59:22 -0400

    typo

--- wpwatcher/__main__.py
+++ wpwatcher/__main__.py
@@ -1,4 +1,4 @@
 from .cli import WPWatcherCLI
-"""Main program if called with wpwatcher/cli.py"""
+"""Main program if called with python3 -m wpwatcher"""
 if __name__ == '__main__':
     WPWatcherCLI()
