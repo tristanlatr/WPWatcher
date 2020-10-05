5250b0903469b47146ec7a1c3ae18e820bdf0ad0
tristanlatr <tris.la.tr@gmail.com>
2020-10-05 14:11:31 -0400

    Bump version

--- wpwatcher/__version__.py
+++ wpwatcher/__version__.py
@@ -7,7 +7,7 @@ DISCLAIMER - USE AT YOUR OWN RISK.
 Project version and meta informations.
 """
 
-__version__ = "2.4.2"
+__version__ = "2.5.0.dev"
 __title__ = "wpwatcher"
 __description__ = "wpwatcher - WordPress Watcher is a Python wrapper for WPScan that manages scans on multiple sites and reports by email and syslog"
 __author__ = "Florian Roth, Tristan Landes"
@@ -15,4 +15,3 @@ __author_email__ = ""
 __license__ = "Apache License 2.0"
 __url__ = "https://github.com/tristanlatr/WPWatcher"
 __keywords__ = "wpscan auto multiple bulk batch scan wordpress email report alerts warnings service automate mass vulnerable sites asynchronous syslog"
-
