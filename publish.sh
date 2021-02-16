7d2db831005567021fab3ede7da05a34415711ba
tristanlatr <tris.la.tr@gmail.com>
2021-02-16 14:06:36 -0500

    Publish to pypi with github action

--- publish.sh
+++ publish.sh
@@ -1,13 +1,3 @@
 #! /bin/bash
-rm -rf ./dist
-rm -fr ./build
-rm -fr ./wpwatcher.egg-info
-python3 setup.py build check sdist bdist_wheel
-python3 -m twine upload --verbose dist/*
-python3 setup.py clean
-rm -rf ./dist
-rm -fr ./build
-rm -fr ./wpwatcher.egg-info
-
 git tag $(python3 setup.py -V)
 git push --tags
