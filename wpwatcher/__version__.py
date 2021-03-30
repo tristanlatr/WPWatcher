a584c8f4d6decf5dba9a1f9f3429fc40b5ef5535
tristanlatr <tris.la.tr@gmail.com>
2021-03-30 16:30:58 -0400

    Bump to version 3.0.1

--- wpwatcher/__version__.py
+++ wpwatcher/__version__.py
@@ -5,7 +5,7 @@ Automating WPscan to scan and report vulnerable Wordpress sites
 Project version and meta informations.
 """
 
-__version__ = "3.0.dev"
+__version__ = "3.0.1"
 __title__ = "wpwatcher"
 __description__ = "WPWatcher - Automating WPScan to scan and report vulnerable Wordpress sites"
 __author__ = "Florian Roth, Tristan Landes"
